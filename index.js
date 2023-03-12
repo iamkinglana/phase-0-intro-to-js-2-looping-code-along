04847e38062bf57608c543f054cefde03819f1ea
+ <+>
2023-03-12 20:21:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+
+function writeCards(name, party){
+    const array = [];
+    for (let i = 0; i < name.length; i++){
+        let msg = `Thank you, ${name[i]}, for the wonderful ${party} gift!`;
+        array.push(msg);
+    }
+    return array;
+}
+function countDown(i) {   
+
+    while (i>=0){    
+        console.log(i);
+    --i;
+    }
+}
